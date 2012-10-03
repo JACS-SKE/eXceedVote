c101bdc575bb27febfc76e51e5f2579fe9108356
floydian129 <floydian129@gmail.com>
2012-10-04 00:29:28 +0700

    clear all
    i'm so sad

--- src/com/jacs/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.jacs.constant;
-
-public class Constant {
-	
-	public final static String AUTH_PANEL = "AUTH_PANEL";
-	public final static String TYPE_PANEL = "TYPE_PANEL";
-	public final static String TEAM_PANEL = "TEAM_PANEL";
-	
-}
