29709fc50f32f16505b40c0d9e94c6604015f687
Apiwat Srisirisitthikul <liverpool_apiwat@hotmail.com>
2012-10-03 22:40:25 +0700

    Authentication

--- src/com/jacs/database/exceed/model/Authentication.java
+++ src/com/jacs/database/exceed/model/Authentication.java
@@ -1,8 +1,100 @@
 package com.jacs.database.exceed.model;
 
-public class Authentication {
-	private String id;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Authentication implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
 	private String username;
 	private String password;
-	private String type; 
+	private String type;
+	public Authentication()	{
+		this("","","");
+	}
+	public Authentication(String username, String password,String type) {
+		this.username = username;
+		this.password = password;
+		this.type = type;
+	}
+	public Integer getId() {
+		return id;
+	}
+	private void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getType() {
+		return type;
+	}
+	private void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Authentication))
+			return false;
+		Authentication other = (Authentication) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Authentication [id=" + id + ", username=" + username
+				+ ", password=" + password + ", type=" + type + "]";
+	} 
 }
