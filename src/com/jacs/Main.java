07a4ee14d747a7ea1df92efef958187e8b66bc78
Apiwat Srisirisitthikul <liverpool_apiwat@hotmail.com>
2012-10-08 11:15:21 +0700

    add auth

--- src/com/jacs/Main.java
+++ src/com/jacs/Main.java
@@ -5,8 +5,11 @@ import com.jacs.gui.MainGUI;
 public class Main {
 
 	public static void main(String[] args) {
-		MainGUI gui = new MainGUI();
-		gui.run();
+		// MainGUI gui = new MainGUI();
+		// gui.run();
+		
+		AuthenticationJPADemo  auth = new AuthenticationJPADemo();
+		auth.runAuthenticationDemo();
 	}
 
 }
--- src/com/jacs/database/exceed/model/Authentication.java
+++ src/com/jacs/database/exceed/model/Authentication.java
@@ -11,7 +11,8 @@ import javax.persistence.Id;
 public class Authentication implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String username;
