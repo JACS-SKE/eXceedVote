78a161de5488227b2705ff6fd6eb4884421f3c01
floydian129 <floydian129@gmail.com>
2012-11-16 18:34:20 +0700

    add CatGUI

---
+++ src/jacs/gui/CatGUI.java
@@ -0,0 +1,13 @@
+package jacs.gui;
+
+import jacs.component.BasePanel;
+import jacs.controller.MainController;
+
+public class CatGUI extends BasePanel {
+
+	public CatGUI(String name, MainController controller) {
+		super(name, controller);
+		
+	}
+
+}
